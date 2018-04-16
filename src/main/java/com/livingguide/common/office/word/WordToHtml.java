dda116d7eafcd8d1f01db3c2df0489008acf08d5
Lele <Lele@172.16.17.103>
2018-04-16 15:58:29 +0800

    持续开发

--- src/main/java/com/livingguide/common/office/word/WordToHtml.java
+++ src/main/java/com/livingguide/common/office/word/WordToHtml.java
@@ -9,17 +9,18 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.usermodel.CharacterRun;
+import org.apache.poi.hwpf.usermodel.Paragraph;
 import org.apache.poi.hwpf.usermodel.Range;
 import org.apache.poi.poifs.filesystem.FileMagic;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 
 public class WordToHtml {
-	
+
 	private File dir;
-	
+
 	private File file;
 
 	private static final String[] EXTENSIONS = new String[] { "doc", "docx" };// and other formats you need
@@ -35,7 +36,7 @@ public class WordToHtml {
 			return (false);
 		}
 	};
-	
+
 	public WordToHtml(String inputDir) throws Exception {
 		this.dir = new File(inputDir);
 		// make sure it's a directory
@@ -50,7 +51,7 @@ public class WordToHtml {
 	}
 
 	public static void main(String argv[]) {
-		String inputFile = "E:\\文档\\资料\\doc";
+		String inputFile = "E:\\文档\\资料\\doc\\已处理";
 		try {
 			new WordToHtml(inputFile);
 		} catch (Exception e) {
@@ -58,88 +59,71 @@ public class WordToHtml {
 		}
 	}
 
-	/**
-	 * 读取每个文字样式
-	 * 
-	 * @param fileName
-	 * @throws Exception
-	 */
-
 	private void getWordAndStyle() throws Exception {
 
-		int  i = 0;
+		int i = 0;
 		for (final File f : dir.listFiles(IMAGE_FILTER)) {
 			FileInputStream in = new FileInputStream(f);
-			
+
 			InputStream is = FileMagic.prepareToCheckMagic(in);
 			FileMagic fm = FileMagic.valueOf(is);
 			String name = f.getName();
-			if (fm == FileMagic.OLE2 && name.substring(name.length()-3).equalsIgnoreCase("doc")) {
-//				analysisDoc(fileName);
-			} else if (fm == FileMagic.OOXML && name.substring(name.length()-4).equalsIgnoreCase("docx")) {
-//				analysisDocx(fileName);
+			if (fm == FileMagic.OLE2 && name.substring(name.length() - 3).equalsIgnoreCase("doc")) {
+				analysisDoc(f.getPath());
+			} else if (fm == FileMagic.OOXML && name.substring(name.length() - 4).equalsIgnoreCase("docx")) {
+				analysisDocx(f.getPath());
 			} else {
 				System.out.println("=========== 暂时无法解析 =====" + fm + "=====" + name);
 				i++;
-//				if (f.exists()) {
-//					f.createNewFile();
-//					System.out.println(f.delete());
-//				}
+				if (f.exists()) {
+					f.createNewFile();
+					System.out.println(f.delete());
+				}
 			}
 		}
 		System.out.println(i);
 	}
-	
+
 	private Map<String, Questions> analysisDoc(String fileName) throws IOException {
+		System.out.println(fileName);
 		FileInputStream in = new FileInputStream(new File(fileName));
 
+		@SuppressWarnings("resource")
 		HWPFDocument doc = new HWPFDocument(in);
-
-		// 取得文档中字符的总数
-		int length = doc.characterLength();
-		// 试题集容器
-		Map<String, Questions> map = new LinkedHashMap<String, Questions>();
-
-		String question = null;
-		String answer = null;
-		String analysis = null;
+		Range range = doc.getRange();
+		int num = range.numParagraphs();
+		String question = "";
+		String answer = "";
+		String analysis = "";
 		String tempStr = "";
-		for (int i = 0; i < length - 1; i++) {
-			// 整篇文章的字符通过一个个字符的来判断,range为得到文档的范围
-			Range range1 = new Range(i, i + 1, doc);
-			Range range2 = new Range(i + 1, i + 2, doc);
-
-			CharacterRun cr = range1.getCharacterRun(0);
-
-			// 第二个字符
-			CharacterRun cr2 = range2.getCharacterRun(0);
-
-			// 比较前后2个字符是否具有相同的格式
-			boolean flag = compareCharStyle(cr, cr2);
-			if (flag) {
-				tempStr += cr.text();
-			} else {
-				boolean isItalic = cr.isItalic();
-				boolean isBold = cr.isBold();
-				if (isBold) {
-					if (question == null || question.length() ==0) {
-						question = tempStr;
-						tempStr = "";
-						continue;
-					}
-					if (question != tempStr) {
-						Questions questions = new Questions(question, answer, analysis);
-						map.put(question, questions);
-						question = tempStr;
-						tempStr = "";
-					}
-				} else if (isItalic){
-					analysis = tempStr;
+		Map<String, Questions> map = new LinkedHashMap<String, Questions>();
+		for (int i = 0; i < num; i++) {
+			Paragraph para = range.getParagraph(i);
+			tempStr = para.text().trim();
+			if (StringUtils.isBlank(tempStr)) {
+				continue;
+			}
+			if (para.getCharacterRun(0).isBold()) {
+				if (question == null || question.length() == 0) {
+					question = tempStr;
 					tempStr = "";
-				} else if (!isBold && !isItalic){
-					answer = tempStr;
-					tempStr = "";
+					continue;
 				}
+				if (question != tempStr) {
+					Questions questions = new Questions(question, answer, analysis);
+					map.put(question, questions);
+					question = tempStr;
+					tempStr = "";
+					analysis = "";
+					answer = "";
+					continue;
+				}
+			} else if (para.getCharacterRun(0).isItalic()) {
+				analysis += tempStr;
+				continue;
+			} else {
+				answer += tempStr;
+				continue;
 			}
 		}
 		if (in != null) {
@@ -155,32 +139,46 @@ public class WordToHtml {
 		return map;
 	}
 
-	private boolean compareCharStyle(CharacterRun cr1, CharacterRun cr2) {
-		boolean flag = false;
-		if (cr1.isBold() == cr2.isBold() && cr1.isItalic() == cr2.isItalic()) {
-			flag = true;
-		}
-		return flag;
-	}
-
-	/**
-	 * 通过XWPFDocument对内容进行访问。对于XWPF文档而言，用这种方式进行读操作更佳。
-	 * 
-	 * @throws Exception
-	 */
-	private void analysisDocx(String fileName) throws Exception {
+	private Map<String, Questions> analysisDocx(String fileName) throws Exception {
 		FileInputStream in = new FileInputStream(new File(fileName));
 		@SuppressWarnings("resource")
 		XWPFDocument doc = new XWPFDocument(in);
 		List<XWPFParagraph> paras = doc.getParagraphs();
+		String question = "";
+		String answer = "";
+		String analysis = "";
+		String tempStr = "";
+		Map<String, Questions> map = new LinkedHashMap<String, Questions>();
 		for (XWPFParagraph para : paras) {
 			if (para.isEmpty()) {
 				continue;
 			}
-			para.getRuns().get(0).isBold();
-			// 当前段落的属性
-			// CTPPr pr = para.getCTP().getPPr();
-			System.out.println(para.getText());
+			tempStr = para.getText().trim();
+			if (StringUtils.isBlank(tempStr)) {
+				continue;
+			}
+			if (para.getRuns().get(0).isBold()) {
+				if (question == null || question.length() == 0) {
+					question = tempStr;
+					tempStr = "";
+					continue;
+				}
+				if (question != tempStr) {
+					Questions questions = new Questions(question, answer, analysis);
+					map.put(question, questions);
+					question = tempStr;
+					tempStr = "";
+					analysis = "";
+					answer = "";
+					continue;
+				}
+			} else if (para.getRuns().get(0).isItalic()) {
+				analysis += tempStr;
+				continue;
+			} else {
+				answer += tempStr;
+				continue;
+			}
 		}
 		if (in != null) {
 			try {
@@ -189,6 +187,10 @@ public class WordToHtml {
 				e.printStackTrace();
 			}
 		}
+		for (String ie : map.keySet()) {
+			System.out.println(map.get(ie).toString());
+		}
+		return map;
 	}
 
 }
